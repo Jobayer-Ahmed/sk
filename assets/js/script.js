45d0dfa0af1db1d82674238bf08ff2bdbe9e8bb6
Jobayer Ahmed Mickey <jobayeramickey@gmail.com>
2018-02-12 12:44:52 +0600

    Fix Bug and Add features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,18 @@ $(document).ready(function() {
 	$('.nav_logo a').click(function() {
 		$('.nav_item').toggleClass('hide')
 	});
-	console.log($('.particles-js').height())
+	$('.nav_item a').click(function() {
+		$('.nav_item').toggleClass('hide')
+	});
+	// $('.nav_item a').click(function () {
+ //    	window.location.href = window.location.href.replace(/#.*/,'/')
+	// });
+	$('.bullet_button_group button').click(function() {
+		$('.bullet_button_group button').removeClass('active')
+		$(this).addClass('active')
+	})
+	$('.third_page .button_group button').click(function() {
+		$('.third_page .button_group button').removeClass('cat_active');
+		$(this).addClass('cat_active')
+	})
 });
